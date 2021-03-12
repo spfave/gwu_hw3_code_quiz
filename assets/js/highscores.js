17149f9e39c8e0d338b47709c6779d12cb137731
Sebastian F <sebpfave2@gmail.com>
2021-03-12 18:07:29 -0500

    Define getScores func

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -2,13 +2,27 @@
 // Render table of scores
 const displayScores = () => {
   const scores = getScores();
+  console.log(scores);
 };
 
 // Create and add score to scores table
 const displayScore = () => {};
 
 // Get scores from local storage
-const getScores = () => {};
+const getScores = () => {
+  // Get scores stored in local storage
+  let scores = localStorage.getItem("scores");
+
+  // if: scores does not exist return empty list
+  // else: return list of scores
+  if (scores === null) {
+    scores = [];
+  } else {
+    scores = JSON.parse(scores);
+  }
+
+  return scores;
+};
 
 // DOM CONTROL:
 // Dom Selectors
