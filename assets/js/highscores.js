2d713045a320cc94f72d65b587354a5e834d8f18
Sebastian F <sebpfave2@gmail.com>
2021-03-12 18:00:22 -0500

    Define starting functionality of highscores page

---
+++ assets/js/highscores.js
@@ -0,0 +1,18 @@
+// FUNCTIONS:
+// Render table of scores
+const displayScores = () => {
+  const scores = getScores();
+};
+
+// Create and add score to scores table
+const displayScore = () => {};
+
+// Get scores from local storage
+const getScores = () => {};
+
+// DOM CONTROL:
+// Dom Selectors
+const scoresTable = document.getElementById("scores-table");
+
+// WEBPAGE EXECUTION:
+displayScores();
